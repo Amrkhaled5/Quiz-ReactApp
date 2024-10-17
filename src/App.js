5d6ff52d46b514a3323227a1cc53c8022e12fc63
amrr <amrr>
2024-10-17 17:51:36 +0300

    base of the App Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import DataCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Questions from "./Questions";
+import { type } from "@testing-library/user-event/dist/type";
+const intialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "gotData":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFaild":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, intialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "gotData", payload: data }))
+      .catch((err) => dispatch({ type: "dataFaild" }));
+  }, []);
+  const { questions, status, index } = state;
   return (
-    <div className="App">
-      <div>
-        <DataCounter />
-      </div>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numOfQuestions={questions.length} dispatch={dispatch} />
+        )}
+        {status === "active" && <Questions question={questions[index]} />}
+      </Main>
     </div>
   );
 }
